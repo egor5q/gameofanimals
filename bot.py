81f04bb0a89314a1cf9349d89e52e2793cc34384
egor5q <31898567+egor5q@users.noreply.github.com>
2018-03-18 12:59:58 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,8 +12,7 @@ from emoji import emojize
 token = os.environ['TELEGRAM_TOKEN']
 bot = telebot.TeleBot(token)
 
-client=MongoClient('mongodb://egor5q:q10egor50010@db-shard-00-00-fej0s.mongodb.net:27017,db-shard-00-01-fej0s.mongodb.net:27017,db-shard-00-02-fej0s.mongodb.net:27017/test?ssl=true&replicaSet=DB-shard-0&authSource=admin')
-
+client=MongoClient('mongodb+srv://egor5q:123@db-fej0s.mongodb.net/test')
         
         
 if __name__ == '__main__':
