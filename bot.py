b563f4648a6ccbdcc56528773b0efb1c393ede0f
egor5q <31898567+egor5q@users.noreply.github.com>
2018-03-18 13:40:55 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,10 +15,15 @@ token = os.environ['TELEGRAM_TOKEN']
 bot = telebot.TeleBot(token)
 
 client=MongoClient('mongodb+srv://egor5q:123@db-fej0s.mongodb.net/test')
+db=client.db1
+collection=db.coll
+        
+        
 
-        
-        
-        
+
+
+
+
         
 if __name__ == '__main__':
   bot.polling(none_stop=True)
