a19fb55e87f121f544a9e88470bd0eb555a4e561
egor5q <31898567+egor5q@users.noreply.github.com>
2018-03-19 09:53:03 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -99,7 +99,7 @@ def feed():
     food=threading.Timer(30, feed)
     food.start()
     mob.update({}, {'$inc':{'food':-1}}) 
-    x=mob.find_one({'token':1})
+    x=mob.find_one({'attack':1})
     print(x['food'])
     
         
